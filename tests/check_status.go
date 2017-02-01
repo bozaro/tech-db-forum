e7851cfd21e08583289620a2f3178b19b3966665
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-02-01 22:24:34 +0300

    Тесты для: status (fix #9)

---
+++ tests/check_status.go
@@ -0,0 +1,52 @@
+package tests
+
+import (
+	"github.com/bozaro/tech-db-forum/generated/client"
+	"github.com/bozaro/tech-db-forum/generated/client/operations"
+)
+
+func init() {
+	Register(Checker{
+		Name:        "status",
+		Description: "",
+		FnCheck:     CheckStatus,
+		Deps: []string{
+			"thread_create_simple",
+		},
+	})
+}
+
+func CheckStatus(c *client.Forum) {
+	result, err := c.Operations.Status(operations.NewStatusParams().
+		WithContext(Expected(200, nil, nil)))
+	CheckNil(err)
+
+	status := result.Payload
+	// Add single user
+	user := CreateUser(c, nil)
+	status.User++
+	_, err = c.Operations.Status(operations.NewStatusParams().
+		WithContext(Expected(200, &status, nil)))
+	CheckNil(err)
+
+	// Add forum
+	forum := CreateForum(c, nil, user)
+	status.Forum++
+	_, err = c.Operations.Status(operations.NewStatusParams().
+		WithContext(Expected(200, &status, nil)))
+	CheckNil(err)
+
+	// Add thread
+	thread := CreateThread(c, nil, forum, user)
+	status.Thread++
+	_, err = c.Operations.Status(operations.NewStatusParams().
+		WithContext(Expected(200, &status, nil)))
+	CheckNil(err)
+
+	// Add post
+	CreatePost(c, nil, thread)
+	status.Post++
+	_, err = c.Operations.Status(operations.NewStatusParams().
+		WithContext(Expected(200, &status, nil)))
+	CheckNil(err)
+}
