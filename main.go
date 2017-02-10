84588059e3635fddff1e1d8277abbcc10b824640
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-02-10 14:33:17 +0300

    Подготовка к создания комманды для запуска наполнения базы

--- main.go
+++ main.go
@@ -2,26 +2,68 @@ package main
 
 //go:generate swagger generate client --target generated --spec ./swagger.yml
 import (
+	"fmt"
 	"github.com/bozaro/tech-db-forum/tests"
 	"github.com/mkideal/cli"
 	"net/url"
 	"os"
+	"reflect"
 )
 
-type argT struct {
+type parserUrl struct {
+	ptr interface{}
+}
+
+func newParserUrl(ptr interface{}) cli.FlagParser {
+	return &parserUrl{ptr}
+}
+
+func (parser *parserUrl) Parse(s string) error {
+	u, err := url.Parse(s)
+	if err == nil {
+		val := reflect.ValueOf(parser.ptr)
+		val.Elem().Set(reflect.ValueOf(*u))
+	}
+	return err
+}
+
+type CmdCommonT struct {
 	cli.Helper
-	ApiUrl string `cli:"url" usage:"base url for testing API" dft:"http://localhost:5000/api"`
+	Url *url.URL `cli:"url" usage:"base url for testing API" parser:"url" dft:"http://localhost:5000/api"`
+}
+
+var root = &cli.Command{
+	Desc: "https://github.com/bozaro/tech-db-forum",
+	Argv: func() interface{} { return nil },
+	Fn: func(ctx *cli.Context) error {
+		ctx.WriteUsage()
+		os.Exit(1)
+		return nil
+	},
+}
+
+type CmdFuncT struct {
+	CmdCommonT
+}
+
+var cmdFunc = &cli.Command{
+	Name: "func",
+	Desc: "run functional testing",
+	Argv: func() interface{} { return new(CmdFuncT) },
+	Fn: func(ctx *cli.Context) error {
+		argv := ctx.Argv().(*CmdFuncT)
+		os.Exit(tests.Run(argv.Url))
+		return nil
+	},
 }
 
 func main() {
-	cli.Run(new(argT), func(ctx *cli.Context) error {
-		argv := ctx.Argv().(*argT)
+	cli.RegisterFlagParser("url", newParserUrl)
 
-		u, err := url.Parse(argv.ApiUrl)
-		if err != nil {
-			return err
-		}
-		os.Exit(tests.Run(u))
-		return nil
-	})
+	if err := cli.Root(root,
+		cli.Tree(cmdFunc),
+	).Run(os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
