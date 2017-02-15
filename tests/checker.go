03e95af13afc99ebb59cdd9ba8075e990e099826
Artem V. Navrotskiy <bozaro@yandex.ru>
2017-02-15 13:10:55 +0300

    Сделал пропуск тестов, которые не могут пройти по зависимостям

--- tests/checker.go
+++ tests/checker.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"sort"
 )
 
 type Checker struct {
@@ -19,6 +20,12 @@ type Checker struct {
 	// Тесты, без которых проверка не имеет смысл.
 	Deps []string
 }
+
+type CheckerByName []Checker
+
+func (a CheckerByName) Len() int           { return len(a) }
+func (a CheckerByName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a CheckerByName) Less(i, j int) bool { return a[i].Name < a[j].Name }
 
 type CheckerTransport struct {
 	t      runtime.ClientTransport
@@ -35,10 +42,10 @@ func Checkpoint(c *client.Forum, message string) bool {
 	return c.Transport.(*CheckerTransport).report.Checkpoint(message)
 }
 
-var checks []Checker
+var registeredChecks []Checker
 
 func Register(checker Checker) {
-	checks = append(checks, checker)
+	registeredChecks = append(registeredChecks, checker)
 }
 
 func RunCheck(check Checker, report *Report, cfg *client.TransportConfig) {
@@ -52,16 +59,70 @@ func RunCheck(check Checker, report *Report, cfg *client.TransportConfig) {
 	check.FnCheck(client.New(&CheckerTransport{transport, report}, nil))
 }
 
+func SortedChecks() []Checker {
+	pending := map[string]Checker{}
+	for _, check := range registeredChecks {
+		if _, ok := pending[check.Name]; ok {
+			log.Fatal("Found duplicate check:", check.Name)
+		}
+		pending[check.Name] = check
+	}
+
+	result := []Checker{}
+	added := map[string]bool{}
+	for len(pending) > 0 {
+		batch := []Checker{}
+		// Found ready tasks
+		for _, item := range pending {
+			ready := true
+			for _, dep := range item.Deps {
+				if !added[dep] {
+					ready = false
+					break
+				}
+			}
+			if ready {
+				batch = append(batch, item)
+			}
+		}
+		if len(batch) == 0 {
+			log.Fatal("Can't found dependencies for tasks:", pending)
+		}
+		// Sort batch by name
+		sort.Sort(CheckerByName(batch))
+		// Add ready tasks to result
+		for _, item := range batch {
+			added[item.Name] = true
+			delete(pending, item.Name)
+		}
+		result = append(result, batch...)
+	}
+
+	return result
+}
+
 func Run(url *url.URL) int {
 	total := 0
 	failed := 0
 	skipped := 0
+	broken := map[string]bool{}
 
 	cfg := client.DefaultTransportConfig().WithHost(url.Host).WithSchemes([]string{url.Scheme}).WithBasePath(url.Path)
-	for _, check := range checks {
+	for _, check := range SortedChecks() {
 		log.Printf("=== RUN:  %s", check.Name)
 		report := Report{}
-		RunCheck(check, &report, cfg)
+		skip := ""
+		for _, dep := range check.Deps {
+			if broken[dep] {
+				skip = dep
+				break
+			}
+		}
+		if skip == "" {
+			RunCheck(check, &report, cfg)
+		} else {
+			report.Skip("Skipped by " + skip)
+		}
 		if report.result != REPORT_SUCCESS {
 			report.Show()
 		}
@@ -69,11 +130,13 @@ func Run(url *url.URL) int {
 		total++
 		switch report.result {
 		case REPORT_SKIPPED:
+			broken[check.Name] = true
 			skipped++
 			result = "SKIPPED"
 		case REPORT_SUCCESS:
 			result = "OK"
 		default:
+			broken[check.Name] = true
 			failed++
 			result = "FAILED"
 		}
